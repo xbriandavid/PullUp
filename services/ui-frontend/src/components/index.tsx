804f6911ddb088dd8bee696a02e3a2a70179405a
Brian <xbriandavid@gmail.com>
2021-01-15 21:07:19 -0800

    implemented routing

--- services/ui-frontend/src/components/index.tsx
+++ services/ui-frontend/src/components/index.tsx
@@ -1,7 +1,11 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import {App} from "./App"
+import {BrowserRouter} from "react-router-dom"
+import Routing from "./Routing"
 
 ReactDOM.render(
-    <App />, document.getElementById("output")
+    <BrowserRouter>
+        <Routing />
+    </BrowserRouter>, 
+    document.getElementById("output")
 )
