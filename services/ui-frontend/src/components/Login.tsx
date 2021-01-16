49208d97694b8f33a6bc7eaf86aff337f850ae3f
Brian <xbriandavid@gmail.com>
2021-01-15 21:07:28 -0800

    initialized login screen

---
+++ services/ui-frontend/src/components/Login.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import {Component} from "react"
+
+
+export default class Login extends Component{
+    render(){
+        return(
+            <div id = "container">
+                <aside id="aside">
+                    <h2>this is the aside</h2>
+                </aside>
+                <h1 id="maintext">this is a test!</h1>
+            </div>
+
+        )
+    }
+}
