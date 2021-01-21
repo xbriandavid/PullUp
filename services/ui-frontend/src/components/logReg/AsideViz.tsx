fdcbbf706b9eb8f51a599495bcf43edeab8b93c1
Brian <xbriandavid@gmail.com>
2021-01-20 22:33:45 -0800

    created component for aside

---
+++ services/ui-frontend/src/components/logReg/AsideViz.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import {Component} from "react"
+import "../styles.css"
+
+export default function AsideViz(){
+    return(
+        <aside id="visual-subcontainer"></aside>
+    )
+}
