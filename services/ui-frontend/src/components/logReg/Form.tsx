2fa19caa64fe594040fa9598baf4e1ad5264bb7f
Brian <xbriandavid@gmail.com>
2021-01-20 22:34:15 -0800

    created skeleton for form component

---
+++ services/ui-frontend/src/components/logReg/Form.tsx
@@ -0,0 +1,18 @@
+import { type } from "os"
+import * as React from "react"
+import {Component, FunctionComponent} from "react"
+
+type FormProps = {
+    user_status: Boolean
+}
+
+export default function Form({
+    user_status
+}: FormProps){
+    if(user_status){
+        
+    } else{
+        
+    }
+    
+}
