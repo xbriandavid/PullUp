cd8add7b9c74ebd0c44a9190080fb06497b79f7b
Brian <xbriandavid@gmail.com>
2021-01-23 21:27:44 -0800

    modified for react bootstrap functionality

--- services/ui-frontend/src/components/index.tsx
+++ services/ui-frontend/src/components/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import {BrowserRouter} from "react-router-dom"
 import Routing from "./Routing"
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
     <BrowserRouter>
