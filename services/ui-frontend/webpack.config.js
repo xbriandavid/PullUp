f59337f9c2fd91b2fc1710f0df8acd68f090935c
Brian <xbriandavid@gmail.com>
2021-01-23 21:28:28 -0800

    added new loaders

--- services/ui-frontend/webpack.config.js
+++ services/ui-frontend/webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
           test: /\.css$/,
           use: ["style-loader", "css-loader"],
         },
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        },
       ]
     },
     devServer: {
