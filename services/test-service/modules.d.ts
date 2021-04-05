e21ced208eb04f0bfd68b0be7b62933ebffed019
Brian <xbriandavid@gmail.com>
2021-04-05 00:35:12 -0700

    namespace and interfaces for JWT

--- services/test-service/modules.d.ts
+++ services/test-service/modules.d.ts
@@ -13,4 +13,9 @@ declare namespace JWT{
     export interface JWTpay{
         iss: string
     }
+    export interface JWT_ID{
+        token: string,
+        issID: string
+    }
+    export type RegisterParams = string | qs.ParsedQs | string[] | qs.ParsedQs[] | undefined
 }
