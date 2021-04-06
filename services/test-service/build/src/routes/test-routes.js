72732a3d27449a2596b56680e3fdb18386efbc17
Brian <xbriandavid@gmail.com>
2021-04-05 23:14:04 -0700

    completed initial registration api pipeline

--- services/test-service/build/src/routes/test-routes.js
+++ services/test-service/build/src/routes/test-routes.js
@@ -11,8 +11,12 @@ var uuid = require('uuid');
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
 var axios = require('axios');
-var privateTok = fs.readFileSync("/Users/fetch/Projects/Pull-up/PullUp/services/test-service/src/private.pem", { encoding: "utf8" });
-// JWT Creation method
+var publicToken = fs.readFileSync("/Users/fetch/Projects/Pull-up/PullUp/services/test-service/src/public.pem", { encoding: "utf8" });
+var privateToken = fs.readFileSync("/Users/fetch/Projects/Pull-up/PullUp/services/test-service/src/private.pem", { encoding: "utf8" });
+var CreateFirebaseUser = function (email, password) {
+    firebase_1.default.auth().createUserWithEmailAndPassword(email, password)
+        .catch(function (error) { return console.log(error); });
+};
 var CreateJWTWithID = function () {
     var payload_iss = "" + uuid.v4();
     var JWTResponseHeader = {
@@ -22,37 +26,52 @@ var CreateJWTWithID = function () {
     var JWTPayload = {
         iss: payload_iss
     };
-    var TokenForJWT = jwt.sign(JWTPayload, privateTok, JWTResponseHeader);
+    var TokenForJWT = jwt.sign(JWTPayload, privateToken, JWTResponseHeader);
     return { token: TokenForJWT, issID: payload_iss };
 };
-// Creating a consumer object for Kong Gateway
-var CreateConsumer = function (email) {
-    //
-    // Create object
-    // use uiid
+//todo - check that email hasn't been used
+var CreateConsumer = function (email, uuid) {
+    axios.post('http://localhost:8001/consumers/', {
+        username: "" + email
+    }).then(function (response) {
+        axios.post("http://localhost:8001/consumers/" + email + "/jwt/", {
+            rsa_public_key: publicToken,
+            algorithm: "RS256",
+            key: uuid
+        });
+    }).catch(function (error) {
+        console.log(error);
+    });
 };
-// Callback to create a Firebase user
-var CreateFirebaseUser = function (email, password) {
-    firebase_1.default.auth().createUserWithEmailAndPassword(email, password)
-        .catch(function (error) { return console.log(error); });
-};
-exports.itemrouter.get('/register', function (req, res) {
-    var email = req.query.email;
-    var password = req.query.password;
-    CreateFirebaseUser("" + email, "" + password);
-    res.send("connection received.\nYour email: " + email);
-    res.status(200);
-});
-exports.itemrouter.post('/makeCustomer', function (req, res) {
+var CreateConsumerCredentials = function (email, uuid) {
     try {
-        axios.post('http://localhost:8001/consumers/', {
-            username: "testusername"
+        axios.post("http://localhost:8001/consumers/" + email + "/jwt/", {
+            rsa_public_key: publicToken,
+            algorithm: "RS256",
+            key: "" + uuid
         });
     }
     catch (error) {
         console.log(error);
     }
-    res.send("request completed");
+};
+/*
+* Creates a JWT token.
+* Creates a firebase user instance. Token creation returns
+* an object that contains a uuid used to verify an account
+* on Kong Gateway consumers
+*/
+exports.itemrouter.get('/register', function (req, res) {
+    var _a = CreateJWTWithID(), JWT = _a["token"], UserUUID = _a["issID"];
+    var email = req.query.email;
+    var password = req.query.password;
+    CreateFirebaseUser("" + email, "" + password);
+    CreateConsumer("" + email, UserUUID);
+    //CreateConsumerCredentials(`${email}`, UserUUID)
+    res.cookie('Clastics', JWT, {
+        httpOnly: true
+    });
+    res.send('connection received');
     res.status(200);
 });
 //# sourceMappingURL=test-routes.js.map
--- services/test-service/src/routes/test-routes.ts
+++ services/test-service/src/routes/test-routes.ts
@@ -8,7 +8,11 @@ const axios = require('axios')
 const publicToken = fs.readFileSync("/Users/fetch/Projects/Pull-up/PullUp/services/test-service/src/public.pem", {encoding: "utf8"})
 const privateToken = fs.readFileSync("/Users/fetch/Projects/Pull-up/PullUp/services/test-service/src/private.pem", {encoding: "utf8"})
 
-// JWT Creation method
+const CreateFirebaseUser = (email: string, password:any):void => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => console.log(error))
+}
+
 const CreateJWTWithID = (): JWT.JWT_ID =>{
     const payload_iss: string = `${uuid.v4()}`
     const JWTResponseHeader: JWT.JWTResHead = {
@@ -21,46 +25,36 @@ const CreateJWTWithID = (): JWT.JWT_ID =>{
     const TokenForJWT: string = jwt.sign(JWTPayload, privateToken, JWTResponseHeader)
     return {token: TokenForJWT, issID: payload_iss}
 }
-//todo - check that email hasn't been used
-// Creating a consumer object for Kong Gateway
-const CreateConsumer = (email: string) => {
-    try {
-        axios.post('http://localhost:8001/consumers/', {
-            username: `${email}`
-        })  
-    } catch (error) {
-        console.log(error)
-    }
-}
-const CreateConsumerCredentials = (email: string, uuid: string) => {
-    try {
+
+const CreateConsumer = (email: string, uuid: string) => {
+    axios.post('http://localhost:8001/consumers/', {
+        username: `${email}`
+    }).then((response: any) => { 
         axios.post(`http://localhost:8001/consumers/${email}/jwt/`, {
             rsa_public_key: publicToken,
             algorithm: "RS256",
-            key: `${uuid}`
-        })  
-    } catch (error) {
+            key: uuid
+        })}
+    ).catch((error: any) => {
         console.log(error)
-    }
+    })
 }
 
-// Callback to create a Firebase user
-const CreateFirebaseUser = (email: string, password:any):void => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch((error) => console.log(error))
-}
-
+/*
+* Creates a JWT token.
+* Creates a firebase user instance. Token creation returns
+* an object that contains a uuid used to verify an account
+* on Kong Gateway consumers
+*/
 itemrouter.get('/register', (req: Request, res: Response) => {
     const {"token": JWT, "issID": UserUUID} = CreateJWTWithID()
     const email: JWT.RegisterParams = req.query.email
     const password = req.query.password
-    /*
-     * Creates a firebase user instance. Token creation returns
-     * an object that contains a uuid used to verify an account
-     * on Kong Gateway consumers
-     */
     CreateFirebaseUser(`${email}`, `${password}`)
-    CreateConsumerCredentials(`${email}`, UserUUID)
+    CreateConsumer(`${email}` ,UserUUID)
+    res.cookie('Clastics', JWT, {
+       httpOnly: true
+    })
     res.send('connection received')
     res.status(200)
 })
