40a12e9f631f999584abd5c75656f5e189e666be
Brian <xbriandavid@gmail.com>
2021-04-09 21:44:47 -0700

    Added EmptyStringError message in ErrorCodes

--- services/test-service/modules.d.ts
+++ services/test-service/modules.d.ts
@@ -24,6 +24,16 @@ declare namespace ErrorCodes{
     export interface AuthErrorCodes {
         EmailNotRegistered: string,
         PasswordUnkown: string,
+        EmptyStringError: string,
         GenericError: string
+    }
+}
+
+declare namespace firebase {
+    export interface FirebaseError {
+        code: string,
+        message: string,
+        name: string,
+        stack?: string
     }
 }
