1d91e5994e1a5aeaeebfe2c6a4a5df4a4a16f43f
Brian <xbriandavid@gmail.com>
2021-06-23 16:23:57 -0700

    removed files

--- services/ui-frontend/src/components/logReg/AsideViz.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import {Component} from "react"
-import "../styles.css"
-
-export default function AsideViz(){
-    return(
-        <aside id="visual-subcontainer"></aside>
-    )
-}
--- services/ui-frontend/src/components/styles.css.d.ts
+++
@@ -1,3 +0,0 @@
-export const aside: String
-export const maintext:String
-export const container: String
