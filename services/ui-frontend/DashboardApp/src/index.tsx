0f3417cc4309c6f0f60004277cbd8ad2877c5913
Brian <xbriandavid@gmail.com>
2021-06-23 16:27:59 -0700

    static files for construction

---
+++ services/ui-frontend/DashboardApp/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+// Subcomponent
+import ConstrunctionWarning from "./Construction"
+
+ReactDOM.render(<ConstrunctionWarning />, document.getElementById("output"))
