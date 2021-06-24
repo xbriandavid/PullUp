93f3bee91565e0f03a7025aff04044883a86879a
Brian <xbriandavid@gmail.com>
2021-06-23 17:56:03 -0700

    removal of files from old directory name

--- services/test-service/config.ts
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash')
-const dotenv = require('dotenv')
-dotenv.config()
-const config_file = require('./config.json')
-const defaultConfig = config_file.development
-const environment= process.env["NODE_ENV"]
-export const environmentConfig = config_file[environment]
