e76c783a02347174587531e323dc82b71dc0bbcb
Brian <xbriandavid@gmail.com>
2021-09-09 16:55:03 -0700

    added button components and styles

---
+++ services/ui-frontend/FormApp/src/components/buttons/save/Savebttn.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import "./savebttn.css"
+
+export default function SaveBttn(){
+    return(
+        <button className="save-bttn">
+            Save
+        </button>
+    )
+}
