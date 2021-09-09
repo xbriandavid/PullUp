f6670fdc81e8d3c97282c34f3ae158d40b0eaf21
Brian <xbriandavid@gmail.com>
2021-09-09 15:54:01 -0700

    added router and switch support

--- services/ui-frontend/FormApp/src/index.tsx
+++ services/ui-frontend/FormApp/src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import ParentFrame from "./components/ParentFrame"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 ReactDOM.render(
-    <ParentFrame/>,
+    <Router>
+        <Switch>
+            <Route exact path="/first">
+                <ParentFrame/>
+            </Route>
+        </Switch>
+    </Router>,
+
     document.getElementById("output")
 )
