4935c70e54fb9de6779ae6788ffa83e7fa33a26f
Brian <xbriandavid@gmail.com>
2021-09-14 18:03:13 -0700

    Placeholder component

---
+++ services/ui-frontend/FormApp/src/components/event-unit-creator/Quickview.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import "./styles.css"
+
+const Quickview: React.FC<{}> = () => {
+    return(
+        <div className="Quickview-wrapper">
+            <p className="qvt">qv</p>
+        </div>
+    )
+}
+
+export default Quickview
